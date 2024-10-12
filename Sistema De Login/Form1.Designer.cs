cade55748b50b08753e6d23d79a39bc8abfd570f
vinemoraes43 <user@DESKTOP-QGN0QC6>
2024-10-12 15:06:59 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema De Login/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Sistema_De_Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btLogar = new System.Windows.Forms.Button();
+            this.btRegistrar = new System.Windows.Forms.Button();
+            this.lbMensagemErro = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(237, 147);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(266, 26);
+            this.tbUsername.TabIndex = 0;
+            this.tbUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbUsername_KeyDown);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(237, 179);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(266, 26);
+            this.tbPassword.TabIndex = 1;
+            this.tbPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbUsername_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LOGIN";
+            // 
+            // btLogar
+            // 
+            this.btLogar.Location = new System.Drawing.Point(377, 211);
+            this.btLogar.Name = "btLogar";
+            this.btLogar.Size = new System.Drawing.Size(126, 44);
+            this.btLogar.TabIndex = 3;
+            this.btLogar.Text = "Entrar";
+            this.btLogar.UseVisualStyleBackColor = true;
+            this.btLogar.Click += new System.EventHandler(this.btLogar_Click_1);
+            // 
+            // btRegistrar
+            // 
+            this.btRegistrar.Location = new System.Drawing.Point(237, 211);
+            this.btRegistrar.Name = "btRegistrar";
+            this.btRegistrar.Size = new System.Drawing.Size(126, 44);
+            this.btRegistrar.TabIndex = 4;
+            this.btRegistrar.Text = "Registrar-se";
+            this.btRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // lbMensagemErro
+            // 
+            this.lbMensagemErro.AutoSize = true;
+            this.lbMensagemErro.Location = new System.Drawing.Point(247, 284);
+            this.lbMensagemErro.Name = "lbMensagemErro";
+            this.lbMensagemErro.Size = new System.Drawing.Size(57, 20);
+            this.lbMensagemErro.TabIndex = 8;
+            this.lbMensagemErro.Text = "label3";
+            this.lbMensagemErro.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(730, 384);
+            this.Controls.Add(this.lbMensagemErro);
+            this.Controls.Add(this.btRegistrar);
+            this.Controls.Add(this.btLogar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Sistema de Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbUsername_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btLogar;
+        private System.Windows.Forms.Button btRegistrar;
+        private System.Windows.Forms.Label lbMensagemErro;
+    }
+}
+
---
+++ Sistema De Login/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Sistema_De_Login
+{
+    public partial class Form1 : Form
+    {
+
+        private string correctUsername = "admin";
+        private string correctPassword = "12345";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tbUsername.KeyDown += new KeyEventHandler(tbUsername_KeyDown);
+            tbPassword.KeyDown += new KeyEventHandler(tbUsername_KeyDown);
+
+        }
+
+        // Keydown entre os textos de username e de password e para ir direito
+        // no botão de login
+
+        private void tbUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) 
+            {
+                e.SuppressKeyPress = true;
+
+                if (sender == tbUsername)
+                {
+
+                    if (!string.IsNullOrWhiteSpace(tbUsername.Text))
+                    {
+                        tbPassword.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("O campo de username não pode estar vazio.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+
+                else if (sender == tbPassword)
+                {
+                    btLogar.PerformClick();
+                }
+
+            }
+
+            if(e.KeyCode == Keys.Down)
+            {
+                e.SuppressKeyPress = true;
+
+                if (sender == tbUsername)
+                {
+                    tbPassword.Focus();
+                }
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                e.SuppressKeyPress = true;
+
+                if (sender == tbPassword)
+                {
+                    tbUsername.Focus();
+                }
+            }
+
+
+        }
+
+        private void btLogar_Click_1(object sender, EventArgs e)
+        {
+            // verificação do login
+
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+
+            bool loginSuccess = ValidateLogin(username, password);
+
+            if (loginSuccess)
+            {
+                lbMensagemErro.Visible = false;
+
+                MessageBox.Show("Login bem-sucedido!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+            else
+            {
+                lbMensagemErro.Text = "Usuário ou senha incorretos!";
+                lbMensagemErro.ForeColor = System.Drawing.Color.Red;
+                lbMensagemErro.Visible = true;
+
+            }
+        }
+
+        private bool ValidateLogin(string username, string password) 
+        {
+            string correctUsername = "admin";
+            string correctPassword = "12345";
+
+            return username == correctUsername && password == correctPassword;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
